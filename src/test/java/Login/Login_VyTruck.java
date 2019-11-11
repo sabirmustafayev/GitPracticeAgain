4066ab962132fdd473fa436696e7f2e0cc90cd29
sabir mustafayev <sabirmustafayev86@gmail.com>
2019-11-11 10:28:55 -0500

    Selenium Login Testing

---
+++ src/test/java/Login/Login_VyTruck.java
@@ -0,0 +1,44 @@
+package Login;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Login_VyTruck {
+    public static void main(String[] args) throws Throwable {
+
+        //
+        //WebDriverManager.firefoxdriver().setup();
+        //FirefoxDriver driverObj = new FirefoxDriver();
+
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driverObj = new ChromeDriver();
+//go to url
+        driverObj.navigate().to("https://qa2.vytrack.com/user/login");
+        Thread.sleep(3000);
+// creating userName object
+        WebElement userName = driverObj.findElement(By.id("prependedInput"));
+        userName.sendKeys("storemanager244");
+//creating passWord object
+        WebElement passWord = driverObj.findElement(By.id("prependedInput2"));
+        passWord.sendKeys("UserUser123");
+//creating clickButton object
+        WebElement clickButton = driverObj.findElement(By.id("_submit"));
+        clickButton.click();
+
+        String expectedTitle = "Dashboard";
+        String actualTitle = driverObj.getTitle();
+        System.out.println("Expected tile is "+expectedTitle);
+        System.out.println("Actual title is "+actualTitle);
+
+        if (expectedTitle == actualTitle){
+            System.out.println("Test failed");
+        }else
+            System.out.println("Test passed");
+
+        driverObj.close();
+    }
+}
